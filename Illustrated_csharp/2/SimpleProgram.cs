9ee1c28a30af5a06d66434ea242abb48576cbc05
Nanoha <1371689491@qq.com>
2020-12-24 17:18:42 +0800

    1

---
+++ Illustrated_csharp/2/SimpleProgram.cs
@@ -0,0 +1,12 @@
+using System;           
+
+namespace Simple
+{
+    class Program       // 声明一个类
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi there!");
+        }
+    }
+}
